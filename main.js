a4d7d5351c3d54e35e4d448e2c038cd7948dec14
Igor <dumanschi.igor@gmail.com>
2024-10-09 18:33:24 +0300

    JS_4_HW

---
+++ main.js
@@ -0,0 +1,84 @@
+// for (let num = 0; num <= 10; num++){
+// 	console.log(num)
+// }
+
+// function numering (numbers) {
+// 	for (let i = 0; i <= numbers; i++) {
+// 		console.log(i)
+// 	} if ( numbers !== Number) {
+// 		console.log(`use please the numbers`)
+// 	}
+// }
+
+// numering(10)
+
+// let num1 = 0
+// while(num1 <= 10) {
+// 	console.log(num1);
+// 	num1++;
+// }
+
+// let num2 = 0
+// do {
+// 	console.log(num2)
+// 	num2++
+// } while (num2 <= 10)
+
+// const word = 'javascript'
+
+// for (let i = 0; i !== word.length; i++) {
+// 	console.log(word[i])
+// }
+
+// function splitWord (word) {
+// 	for (let i = 0; i !== word.length; i++) {
+// 		console.log(word[i])
+// 	}
+// }
+
+// splitWord('programing')
+
+// const splitWord1 = function (word1) {
+// 	for (let i = 0; i !== word1.length; i++) {
+// 		console.log(word1[i])
+// 	}
+// }
+
+// splitWord1('dumanschii')
+
+
+
+// const splitWord2 = (word2) => {
+//   if (typeof word2 === 'string') {
+//     for (let i = 0; i !== word2.length && word2 !== Number; i++) {
+//       console.log(word2[i]);
+//     }
+//   }
+//   else {
+//     console.log(`please don't use numbers`);
+//   }
+// };
+
+// splitWord2('functie');
+
+// const adunare = (num1, num2, num3) => {
+// 	if (typeof num1 !== 'string' && typeof num2 !== 'string' && typeof num3 !== 'string') {
+// 	  console.log(num1 + num2 + num3)
+// } else {
+// 	console.log(` introduceti cifre`)
+// }
+// }
+
+// adunare(5, 5, 5)
+
+const driver = {
+	name:'Alex',
+	age:'40',
+	gender:'male',
+	ocupation:'taxi driver',
+	city:'New York'
+}
+
+const driverInfo = () => {
+	console.log(``)
+}
